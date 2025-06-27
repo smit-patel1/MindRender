3ff2ca3ff1efb60ee3db1d4155a835d30cb30fc5
Smit Patel <64547898+smit-patel1@users.noreply.github.com>
2025-06-27 14:44:34 -0400

    Increase simulation size to 98%

--- frontend/src/pages/Demo.tsx
+++ frontend/src/pages/Demo.tsx
@@ -349,8 +349,8 @@ const SimulationIframe = React.memo(({ simulationData }: { simulationData: Simul
           box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1);
           display: block !important;
           margin: 0 auto;
-          max-width: 95%;
-          max-height: 95%;
+          max-width: 98%;
+          max-height: 98%;
           width: auto;
           height: auto;
           cursor: pointer;
